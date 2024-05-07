58da9fad5f3de30db4ff14a32047624017376f77
Denis Hurskiy <denis.gurskiy@gmail.com>
2024-05-07 21:35:33 +0200

    make build

---
+++ index.cd578b6b.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientHeight/2-e.clientHeight/2,l=t.clientWidth/2-e.clientWidth/2;e.style.top="".concat(c,"px"),e.style.left="".concat(l,"px");
+//# sourceMappingURL=index.cd578b6b.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+const height = wall.clientHeight / 2 - spider.clientHeight / 2;
+const left = wall.clientWidth / 2 - spider.clientWidth / 2;
+spider.style.top = `${height}px`;
+spider.style.left = `${left}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
